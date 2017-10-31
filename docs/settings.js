c65b04e009821969f99abf9efed628529aed3a7c
Daniel Beeke <daniel@studiofonkel.nl>
2017-10-31 17:56:14 +0100

    Removing docs again, I am not satisfied with this ghpages structure

--- docs/settings.js
+++
@@ -1,9 +0,0 @@
-export const settings = {
-  iceServers: [{
-    urls: ['turn:connect.opengroup.io'],
-    username: 'lorem',
-    credential: 'ipsum'
-  }, {
-    urls: ['stun:connect.opengroup.io']
-  }]
-};
