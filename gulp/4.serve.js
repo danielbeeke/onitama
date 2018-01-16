ea34086c90c2c3dc1dd635b8677619c8dd2d3d14
Daniel Beeke <daniel.beeke@gmail.com>
2018-01-16 22:27:27 +0100

    fixing gulp and es6 fallback

--- gulp/4.serve.js
+++ gulp/4.serve.js
@@ -2,11 +2,21 @@
 
 let gulp = require('gulp');
 let reload = global.browserSync.reload;
+let url = require('url');
+let fs = require('fs');
 
 gulp.task('browsersync', () => {
   global.browserSync.init({
     server: [global.paths.src],
-    ghostMode: true
+    ghostMode: true,
+    middleware: function(req, res, next) {
+      var fileName = url.parse(req.url).pathname;
+      var fileExists = fs.existsSync('src/' + fileName);
+      if (!fileExists && fileName.indexOf("browser-sync-client") < 0 || fileName === '/') {
+        req.url = '/404.html';
+      }
+      return next();
+    }
   });
 
   gulp.watch([global.paths.html, global.paths.js]).on('change', reload);
